4ce5f1bf197512349a46ba40752e3542787eb0b4
pal_16 <mantrypalak@gmail.com>
2020-05-03 21:16:43 +0530

    palak changes for connection of database

--- main.py
+++ main.py
@@ -12,12 +12,84 @@ from cloudmersive_extract import predict
 from ResumeParser.main import transform
 from text_summariser import generate_summary
 from ResumeAndFeedbackClassifier.test import classify
-
+from flask_mysqldb import MySQL
+from flask import session
 app = Flask(__name__)
-api= Api(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'	
+app.config['MYSQL_PASSWORD'] = 'tata2000@P'
+app.config['MYSQL_DB']	= 'flutter'
+mysql = MySQL(app)
+api = Api(app)
+
 app.config['SECRET_KEY']='mysecretkey'
 
 nlp=spacy.load('en_core_web_sm')
+class UserLogin(Resource):
+
+    def post(self):
+        email =request.form['email']
+        password = request.form['password']
+        print(email)
+        emailt=''
+        for i in email:
+         if i == '@':
+            break
+         else:
+            emailt=emailt+i
+        print(emailt)
+        result=""
+        try:
+            string = "SELECT * FROM "+emailt+" WHERE email = '"+email+"' and password ='"+password+"' "
+            cur = mysql.connection.cursor()
+            cur.execute(string)
+            result = cur.fetchall()
+            print(result)
+        finally:
+            if result == "":
+                session['login']='0'
+                return redirect(url_for('userlogin'))
+            else:
+                print(result)
+                words = result[0][2].split()
+                text = ""
+                for word in words:
+                    text=text+word[0]
+                session['email']=email
+                session['logged_in']=True
+                return redirect(url_for('dashboard', user_name=result[0][2],initials=text),code=307)
+    
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('login.html'),200,headers)
+
+class UserRegister(Resource):
+    
+    def post(self):
+      name = request.form['name']
+      email = request.form['email']
+      password = request.form['password']
+      typee = request.form['type']
+      emailt=''
+      for i in email:
+         if i == '@': 
+            break
+         else:
+            emailt=emailt+i
+      print(emailt) 
+
+      cur = mysql.connection.cursor()
+      temp="CREATE TABLE if not exists "+emailt+" (id int PRIMARY KEY AUTO_INCREMENT, email varchar(200),name varchar(200), password varchar(200),type varchar(200))"
+      cur.execute(temp)
+      flag="INSERT INTO "+emailt+" (email,name,password,type) VALUES(%s,%s,%s,%s)"
+      cur.execute(flag,(email,name,password,typee))
+      mysql.connection.commit()
+      return redirect(url_for('userlogin')) 
+
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('register.html'),200,headers)
+
 
 def serialize_sets(obj):
     if isinstance(obj, set):
@@ -25,12 +97,24 @@ def serialize_sets(obj):
 
     return obj
 
+
+
 class Dashboard(Resource):
     def __init__(self):
         pass
-    def get(self):
+    def post(self):
         headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('index2.html'),200,headers)
+        return make_response(render_template('index2.html',user_name=user_name,initials=initials),200,headers)
+
+    
+    def get(self):
+        if session.get('logged_in') == True:
+            headers = {'Content-Type': 'text/html'}
+            return make_response(render_template('index2.html',user_name="",initials=""),200,headers)
+
+           
+        else:
+            return redirect(url_for('userlogin')) 
 
 class Classifier(Resource):
     def post(self):
@@ -175,10 +259,15 @@ class Inbox(Resource):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('inbox.html'),200,headers)
 
+class Logout(Resource):
+    def get(self):
+        session.pop('logged_in', None)
 
-
+api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
 api.add_resource(Dashboard, '/')
 api.add_resource(Inbox, '/inbox')
+api.add_resource(Inbox, '/logout')
 api.add_resource(Classifier, '/classifier')
 api.add_resource(Resume, '/resume')
 api.add_resource(Sentimental, '/sentimental') #feedback
